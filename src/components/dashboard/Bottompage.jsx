ea6c3a83bc5e972aaf912b26bcd4b62393245d4e
kartik-raj7 <kartikeyrajgupta007@gmail.com>
2023-06-11 12:42:30 +0530

    minor changes

--- src/components/dashboard/Bottompage.jsx
+++ src/components/dashboard/Bottompage.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
-import Traffictile from './Traffictile'
+import React from "react";
+import Traffictile from "./Traffictile";
 
 const Bottompage = () => {
   return (
     <div>
-      <div style={{display:'flex',flexDirection:'row',justifyContent:'space-between',width:'95%',padding:'20px 30px 0px 20px'}}>
-       <Traffictile/>
-       <Traffictile/>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          width: "95%",
+          padding: "5px 30px 0px 20px",
+          marginBottom:'10px'
+        }}
+      >
+        <Traffictile />
+        <Traffictile />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Bottompage
+export default Bottompage;
